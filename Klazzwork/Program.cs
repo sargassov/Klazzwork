35daf36bbf05017099d9f07472fabb07ac8c3a8d
sargassov <sargassov@yandex.ru>
2020-07-11 13:15:22 +0300

    Добавьте файлы проекта.

---
+++ Klazzwork/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klazzwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo symbol;
+            int count = 0;
+            do
+            {
+                symbol = Console.ReadKey();
+                if (symbol.KeyChar == ' ') count++;
+            }
+            while (symbol.KeyChar != '.');
+            
+        }
+    }
+}
